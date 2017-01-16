c541212b6df587095af6f28a23601a05be9fb203
reljapaunovic <paunovic.relja@gmail.com>
2017-01-16 18:57:43 +0200

    ASDASD

--- awsomeProject/views.py
+++ awsomeProject/views.py
@@ -2,11 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
 
-effing = True
 
 def index(request):
-    if effing == True:
+    if Product.objects.filter(pk=1) == None:
         data = Product(title="Panda")
         data.save()
-        effing = False
     return render(request, "index.html", {"title" : Product.objects.values('title').filter(pk=1)[0]['title']})
