501b68e87c62a30d1974e8c201190964c408fcd2
reljapaunovic <paunovic.relja@gmail.com>
2017-01-16 18:53:04 +0200

    HOPEFULLY fixed problem with data

--- awsomeProject/views.py
+++ awsomeProject/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
 
+effing = True
+
 def index(request):
-    #data = Product(title="Panda")
-    #data.save()
+    if effing == True:
+        data = Product(title="Panda")
+        data.save()
+        effing = False
     return render(request, "index.html", {"title" : Product.objects.values('title').filter(pk=1)[0]['title']})
