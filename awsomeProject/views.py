6c49a436b77862076fee88c6fab98600ed0ed31b
reljapaunovic <paunovic.relja@gmail.com>
2017-01-16 19:03:57 +0200

    ASDASD

--- awsomeProject/views.py
+++ awsomeProject/views.py
@@ -4,7 +4,7 @@ from .models import Product
 
 
 def index(request):
-    if Product.objects.filter(pk=1).exists():
+    if not Product.objects.filter(pk=1).exists():
         data = Product(title="Panda")
         data.save()
     return render(request, "index.html", {"title" : Product.objects.values('title').filter(pk=1)[0]['title']})
